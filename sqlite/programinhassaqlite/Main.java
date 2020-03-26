26fa7f98bff45e7abb34cd744d065fe4d6956337
jkalebe <jeremiaskalebe@gmail.com>
2020-03-26 19:23:47 +0000

    up

--- sqlite/programinhassaqlite/Main.java
+++ sqlite/programinhassaqlite/Main.java
@@ -1,7 +1,6 @@
 package sqlite.programinhassaqlite;
 
-import java.sql.Statement;
-import java.lang.System.Logger;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,31 +11,40 @@ public class Main{
     public static void main(String[] args) {
         ConexaoSQLite conexaoSQLite = new ConexaoSQLite();
         
-        ResultSet resultSet = null;
-        Statement statement = null;
-
         conexaoSQLite.conectar();
-        String query = "SELECT * FROM tbl_pessoa;";
 
-        statement = conexaoSQLite.criarStatement();
+        ResultSet resultSet = null;
+        PreparedStatement preparedStatement = null;
 
+        String sql = "SELECT * "
+                    +" FROM tbl_pessoa"
+                    +" WHERE id = ?;";
+        
         try {
-            resultSet = statement.executeQuery(query);
+            
+            int idPessoa = 1;
+            preparedStatement = conexaoSQLite.criarPreparedStatement(sql);
+            preparedStatement.setInt(1, idPessoa);
+
+            resultSet = preparedStatement.executeQuery();
+
             while(resultSet.next()){
-                System.out.println("Dados da pessoa");
-                System.out.println("id = "+resultSet.getInt("id"));
-                System.out.println("nome = "+resultSet.getString("nome"));
-                System.out.println("idade = "+resultSet.getInt("idade"));
-                System.out.println("-----------------------------------------");
+                System.out.println("Pessoa selecionada");
+                System.out.println("ID = " + resultSet.getInt("id"));
+                System.out.println("NOME = " + resultSet.getString("nome"));
+                System.out.println("IDADE = " + resultSet.getInt("idade"));
+
             }
+
         } catch (SQLException e) {
-            System.out.println("Erro misteriosos");
+            e.printStackTrace();
         }finally{
             try {
                 resultSet.close();
-                statement.close();
-            } catch (SQLException ex) {
-                System.out.println("Erro misterioso de fechamento");
+                preparedStatement.close();
+                conexaoSQLite.desconectar();
+            } catch (SQLException ex){
+                ex.printStackTrace();
             }
         }
     }
